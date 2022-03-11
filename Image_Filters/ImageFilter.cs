dcb23b84bfef903605748506784ed276acf74c8b
PrusWielki <prusakpat@gmail.com>
2022-03-11 11:29:25 +0100

    change brightness filter to pixel by pixel (prob wont compile)

--- Image_Filters/ImageFilter.cs
+++ Image_Filters/ImageFilter.cs
@@ -44,33 +44,44 @@ for (int y = 0; (y <= (pic.Height - 1)); y++) {
 
         public override Image applyFilter(Image imgSource)
         {
-            Bitmap bmpDest = new Bitmap(imgSource.Width,
-         imgSource.Height);
 
-            ColorMatrix clrMatrix = new ColorMatrix(new float[][]
-               {
-            new float[] {1, 0, 0, 0, 0},
-            new float[] {0, 1, 0, 0, 0},
-            new float[] {0, 0, 1, 0, 0},
-            new float[] {0, 0, 0, 1, 0},
-            new float[] {0.5F, 0.5F, 0.5F, 0, 1}
-               });
+           int amount=10;
 
-            using (ImageAttributes attrImage = new ImageAttributes())
-            {
+  // GDI+ still lies to us - the return format is BGR, NOT RGB.
+  BitmapData bmData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
 
-                attrImage.SetColorMatrix(clrMatrix);
+  int stride = bmData.Stride;
+  System.IntPtr Scan0 = bmData.Scan0;
 
-                using (Graphics g = Graphics.FromImage(bmpDest))
-                {
-                    g.DrawImage(imgSource, new Rectangle(0, 0,
-                    imgSource.Width, imgSource.Height), 0, 0,
-                    imgSource.Width, imgSource.Height, GraphicsUnit.Pixel,
-                    attrImage);
-                }
-            }
+  int nVal = 0;
 
-            return bmpDest;
+  unsafe
+  {
+    byte* p = (byte*)(void*)Scan0;
+
+    int nOffset = stride - bitmap.Width * 3;
+    int nWidth = bitmap.Width * 3;
+
+    for (int y = 0; y < bitmap.Height; ++y)
+    {
+      for (int x = 0; x < nWidth; ++x)
+      {
+        nVal = (int)(p[0] + amount);
+
+        if (nVal < 0) nVal = 0;
+        if (nVal > 255) nVal = 255;
+
+        p[0] = (byte)nVal;
+
+        ++p;
+      }
+      p += nOffset;
+    }
+  }
+
+  bitmap.UnlockBits(bmData);
+
+  return bitmap;
         }
     }
     class ContrastEnchancement : ImageFilter
